3a9439c9412eafa6edd248f6dd9b50b15ec8f766
unknown <rollerfernandez@gmail.com>
2020-07-05 22:18:26 -0500

    cambios ultimos

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -18,7 +18,7 @@ export default class Header extends Component {
                         <nav className="main-nav" data-menu-gradient>
                             <ul className="menu">
                                 <li className="active">
-                                    <Link to="/">INICIO</Link></li>
+                                    <Link to="/Inicio">INICIO</Link></li>
                                 <li>
                                     <Link to="/">CONCIERTOS <svg xmlns="http://www.w3.org/2000/svg" className="show-sub-menu" viewBox="0 0 129 129" enableBackground="new 0 0 129 129"><g><path d="m121.3,34.6c-1.6-1.6-4.2-1.6-5.8,0l-51,51.1-51.1-51.1c-1.6-1.6-4.2-1.6-5.8,0-1.6,1.6-1.6,4.2 0,5.8l53.9,53.9c0.8,0.8 1.8,1.2 2.9,1.2 1,0 2.1-0.4 2.9-1.2l53.9-53.9c1.7-1.6 1.7-4.2 0.1-5.8z" /></g></svg></Link>
                                     <ul className="sub-menu">

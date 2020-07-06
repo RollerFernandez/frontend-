80e6c7649fa23045f260d2bba5873371b1614125
unknown <rollerfernandez@gmail.com>
2020-07-05 21:25:40 -0500

    Updates

---
+++ precache-manifest.1b79bf4fa61eb47cb43c6c2f6fc9616f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c3595aa27c0dce623b21561d0802f43",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "04d64ac67a8dd465b947",
+    "url": "/frontend/static/css/main.ce9a0946.chunk.css"
+  },
+  {
+    "revision": "0b4f011c751a9d4ffef7",
+    "url": "/frontend/static/js/2.fe61b682.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend/static/js/2.fe61b682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d64ac67a8dd465b947",
+    "url": "/frontend/static/js/main.59f03b25.chunk.js"
+  },
+  {
+    "revision": "747c638a0ed8091beb0a",
+    "url": "/frontend/static/js/runtime-main.12e52466.js"
+  },
+  {
+    "revision": "fffd3a20c824f1612179619fb02192c0",
+    "url": "/frontend/static/media/main-slider-1.fffd3a20.jpg"
+  },
+  {
+    "revision": "a0bd5ba27eab28a0ac610c2266556d4a",
+    "url": "/frontend/static/media/news-1.a0bd5ba2.jpg"
+  },
+  {
+    "revision": "11f1257205b52b15c5f732c1bc108a5d",
+    "url": "/frontend/static/media/news-2.11f12572.jpg"
+  },
+  {
+    "revision": "f3bc398b157a04cebc7532c01762fbb8",
+    "url": "/frontend/static/media/paralax-2.f3bc398b.jpg"
+  },
+  {
+    "revision": "019e9839a8689ca5973ec553c1f0f822",
+    "url": "/frontend/static/media/recording5960.019e9839.svg"
+  },
+  {
+    "revision": "4c2d3f565972d31cbcf53362a3a1257c",
+    "url": "/frontend/static/media/recording5960.4c2d3f56.eot"
+  },
+  {
+    "revision": "4f45472844a2c0f306fa37ea0215367d",
+    "url": "/frontend/static/media/recording5960.4f454728.ttf"
+  },
+  {
+    "revision": "9d0579aff5f3e3971cf2665a2e0d490c",
+    "url": "/frontend/static/media/recording5960.9d0579af.woff"
+  },
+  {
+    "revision": "3db15afdebcefc2fa76551b5ba11c86a",
+    "url": "/frontend/static/media/testimonials-1.3db15afd.jpg"
+  }
+]);

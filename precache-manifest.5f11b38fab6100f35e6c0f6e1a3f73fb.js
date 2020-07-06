a3c676af41838d01ff0b330590c6375f9f6885bb
unknown <rollerfernandez@gmail.com>
2020-07-05 22:29:35 -0500

    Updates

---
+++ precache-manifest.5f11b38fab6100f35e6c0f6e1a3f73fb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422547b0cd2178b435a23e5eba1d1a5b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "41d4d6bca8a4fb9ff8be",
+    "url": "./static/css/main.3b663383.chunk.css"
+  },
+  {
+    "revision": "0b4f011c751a9d4ffef7",
+    "url": "./static/js/2.fe61b682.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.fe61b682.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41d4d6bca8a4fb9ff8be",
+    "url": "./static/js/main.b0d1e7c0.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  },
+  {
+    "revision": "fffd3a20c824f1612179619fb02192c0",
+    "url": "./static/media/main-slider-1.fffd3a20.jpg"
+  },
+  {
+    "revision": "a0bd5ba27eab28a0ac610c2266556d4a",
+    "url": "./static/media/news-1.a0bd5ba2.jpg"
+  },
+  {
+    "revision": "11f1257205b52b15c5f732c1bc108a5d",
+    "url": "./static/media/news-2.11f12572.jpg"
+  },
+  {
+    "revision": "f3bc398b157a04cebc7532c01762fbb8",
+    "url": "./static/media/paralax-2.f3bc398b.jpg"
+  },
+  {
+    "revision": "019e9839a8689ca5973ec553c1f0f822",
+    "url": "./static/media/recording5960.019e9839.svg"
+  },
+  {
+    "revision": "4c2d3f565972d31cbcf53362a3a1257c",
+    "url": "./static/media/recording5960.4c2d3f56.eot"
+  },
+  {
+    "revision": "4f45472844a2c0f306fa37ea0215367d",
+    "url": "./static/media/recording5960.4f454728.ttf"
+  },
+  {
+    "revision": "9d0579aff5f3e3971cf2665a2e0d490c",
+    "url": "./static/media/recording5960.9d0579af.woff"
+  },
+  {
+    "revision": "3db15afdebcefc2fa76551b5ba11c86a",
+    "url": "./static/media/testimonials-1.3db15afd.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/frontend-/precache-manifest.7b3410e07f852366f3f6f583e13f2016.js"
+  "./precache-manifest.5f11b38fab6100f35e6c0f6e1a3f73fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frontend-/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
